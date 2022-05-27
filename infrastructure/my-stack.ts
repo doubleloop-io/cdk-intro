ffc8a9ff171412ee1048f2fb00e296e9294e532e
Andrea Vallotti <andrea.vallotti@gmail.com>
2022-05-27 13:02:04 +0200

    add echo lambda function

--- infrastructure/my-stack.ts
+++ infrastructure/my-stack.ts
@@ -1,4 +1,6 @@
 import {App, Stack} from "aws-cdk-lib"
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import {Runtime} from "aws-cdk-lib/aws-lambda";
 
 const app = new App()
 
@@ -7,4 +9,15 @@ const myStack = new Stack(app, "MyStack", {
         region: process.env.CDK_DEFAULT_REGION,
         account: process.env.CDK_DEFAULT_ACCOUNT,
     }
+})
+
+const lambda = new NodejsFunction(myStack, "echo", {
+    entry: "src/echo-handler.ts",
+    bundling: {
+        externalModules: [
+            "aws-sdk", // Use the 'aws-sdk' available in the Lambda runtime
+        ],
+    },
+    runtime: Runtime.NODEJS_16_X,
+    functionName: "MyStack-echo"
 })
