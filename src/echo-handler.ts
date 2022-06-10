ba2cbc66689a184ab2d2c7f96f4c1bf5298a98bc
Andrea Vallotti <andrea.vallotti@gmail.com>
2022-06-10 12:47:58 +0200

    echo does something visible

--- src/echo-handler.ts
+++ src/echo-handler.ts
@@ -1,3 +1,10 @@
-export const handler = async () => {
+import {APIGatewayProxyEventV2, APIGatewayProxyResultV2} from "aws-lambda";
+
+export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
     console.log("Echo")
+    const name = event.queryStringParameters?.["name"] || "unknown"
+    return {
+        body: `Hi ${name}!`,
+        statusCode: 200
+    }
 }
